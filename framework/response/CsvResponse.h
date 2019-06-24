1060a4e1e6be51acf39ac4518cfd759d66ad437f
myduomilia <myduomilia@gmail.com>
2019-06-24 16:26:47 +0400

    filename csv response

--- framework/response/CsvResponse.h
+++ framework/response/CsvResponse.h
@@ -1,13 +1,15 @@
 #ifndef CSVRESPONSE_H
 #define CSVRESPONSE_H
 
+#include <sstream>
+
 namespace onyx {
 
     class CsvResponse : public BaseResponse {
     public:
 
-        explicit CsvResponse(const std::string & body) :
-        BaseResponse("Content-Disposition: attachment; Content-type: application/csv; charset=utf-8\r\n\r\n") {
+        CsvResponse(const std::string & body) : 
+        BaseResponse("Content-Disposition: attachment; Content-type: application/csv; filename=download.csv; charset=utf-8\r\n\r\n") {
             m_body = body;
         }
     };
