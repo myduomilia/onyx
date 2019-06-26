b1b0bdef4ca21420e3ac314f45982dee29a31ca2
myduomilia <myduomilia@gmail.com>
2019-06-26 18:21:42 +0400

    commit

--- framework/response/CsvResponse.h
+++ framework/response/CsvResponse.h
@@ -9,7 +9,7 @@ namespace onyx {
     public:
 
         CsvResponse(const std::string & body) : 
-        BaseResponse("Content-Disposition: attachment; Content-type: application/csv; filename=download.csv; charset=utf-8\r\n\r\n") {
+        BaseResponse("Content-Disposition: attachment; filename=download.csv\r\nContent-type: application/csv; charset=utf-8\r\n\r\n") {
             m_body = body;
         }
     };
